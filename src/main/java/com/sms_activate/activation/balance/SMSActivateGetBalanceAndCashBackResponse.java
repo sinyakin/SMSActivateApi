5591132547d35873616d86e919f4f6ca56185975
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2020-11-20 17:55:14 +0300

    Documented getBalance

--- src/main/java/com/sms_activate/activation/balance/SMSActivateGetBalanceAndCashBackResponse.java
+++ src/main/java/com/sms_activate/activation/balance/SMSActivateGetBalanceAndCashBackResponse.java
@@ -5,18 +5,34 @@ import org.jetbrains.annotations.NotNull;
 import java.math.BigDecimal;
 
 public class SMSActivateGetBalanceAndCashBackResponse extends SMSActivateGetBalanceResponse {
+  /**
+   * Cashback in account
+   */
   private final BigDecimal cashBack;
 
+  /**
+   * Constructor getBalanceAndCashBack response with data.
+   * @param balance balance in account.
+   * @param cashBack cashback in account.
+   */
   public SMSActivateGetBalanceAndCashBackResponse(@NotNull BigDecimal balance, @NotNull BigDecimal cashBack) {
     super(balance);
     this.cashBack = cashBack;
   }
 
+  /**
+   * Returns the cashback in account.
+   * @return cashback in account
+   */
   @NotNull
   public BigDecimal getCashBack() {
     return cashBack;
   }
 
+  /**
+   * Returns the sum with balance and cashback.
+   * @return sum with balance and cashback.
+   */
   @NotNull
   public BigDecimal getBalanceAndCashBack() {
     return getBalance().add(cashBack);
--- src/main/java/com/sms_activate/activation/balance/SMSActivateGetBalanceResponse.java
+++ src/main/java/com/sms_activate/activation/balance/SMSActivateGetBalanceResponse.java
@@ -5,12 +5,23 @@ import org.jetbrains.annotations.NotNull;
 import java.math.BigDecimal;
 
 public class SMSActivateGetBalanceResponse {
+  /**
+   * Balance in account.
+   */
   private final BigDecimal balance;
 
+  /**
+   * Constructor get balance response.
+   * @param balance balance in account.
+   */
   public SMSActivateGetBalanceResponse(@NotNull BigDecimal balance) {
     this.balance = balance;
   }
 
+  /**
+   * Returns the balance in account.
+   * @return balance in account
+   */
   @NotNull
   public BigDecimal getBalance() {
     return balance;
